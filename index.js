0dc4ffd9d5f624751c41e00b392b357b2e902192
TeddyPickerAC <hangyul010101@gmail.com>
2022-03-28 19:47:47 +0900

    asdf

--- index.js
+++ index.js
@@ -4,11 +4,25 @@ const Intents = Discord.Intents;
 require('dotenv').config();
 const client = new Discord.Client({
 	intents: [
+		Intents.FLAGS.GUILDS,
 		Intents.FLAGS.GUILD_MESSAGES,
-	]
+		Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS
+	],
+	makeCache: Discord.Options.cacheWithLimits({
+		MessageManager: 0,
+		ThreadManager: 0,
+		VoiceStateManager: 0,
+		GuildInviteManager: 0,
+		GulidBanManager: 0,
+		StageInstanceManager: 0,
+		ThreadMemberManager: 0,
+		GuildStickerManager: 0,
+		ReactionUserManager: 0,
+	}),
+	partials: ["CHANNEL"]
 });
 
-client.on("ready", () => {
+client.on('ready', () => {
   console.log(`${client.user.tag}로 로그인 완료!`);
   client.user.setActivity(`이모지 확대용 봇 | ${client.guilds.cache.size}개의 서버에서 일하는중`, {
     type: "PLAYING",
@@ -19,16 +33,21 @@ process.on("unhandledRejection", (reason, p) => {
 	console.log(`error: ${(reason)?.stack ?? util.inspect(reason)}`);
 })
 
-client.on("message", async (message) => {
+client.on('messageCreate', async (message) => {
 	if(message.author.bot || message.reference || 
-	   !String(message.content) || !/^<a?:.+?:\d+>$/.test(String(message.content))) 
+	   !String(message.content) || 
+	   !/^<a?:.+?:\d+>$/.test(String(message.content)) ||
+	   message.content.split(":").length != 3) 
 		return;
   	await message.delete();
 
   	const link = message.content.includes("<a:") ? 
 		`https://cdn.discordapp.com/emojis/${message.content.split(":")[2].split(">")[0]}.gif` :
 		`https://cdn.discordapp.com/emojis/${message.content.split(":")[2].split(">")[0]}.png`;
-	await message.channel.send(`**${message.member.displayName}** :\n`, {files: [link]});
+	await message.channel.send({
+		content: `**${message.member.displayName}** :\n`,
+		files: [link]
+	});
 });
 
 client.on("guildCreate", async (guild) => {
